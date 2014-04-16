79464feb1c8ceff32a5b7ea557c3290ce41a4a7b
swilk19 <swilkins@edi.home>
2014-04-15 21:44:16 -0400

    Additional cleanup after map view

--- gen/com/swilk/locmsgclient/R.java
+++ gen/com/swilk/locmsgclient/R.java
@@ -209,12 +209,13 @@ containing a value of this type.
     }
     public static final class id {
         public static final int TextView01=0x7f050007;
-        public static final int action_settings=0x7f05000d;
         public static final int button1=0x7f05000b;
         public static final int button2=0x7f050009;
         public static final int button3=0x7f05000c;
         public static final int editText1=0x7f050008;
         public static final int hybrid=0x7f050004;
+        public static final int item1=0x7f05000e;
+        public static final int map=0x7f05000d;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int satellite=0x7f050002;
@@ -228,6 +229,7 @@ containing a value of this type.
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int map_view=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f0a0000;
@@ -331,6 +333,7 @@ containing a value of this type.
         public static final int common_signin_button_text_long=0x7f060013;
         public static final int hello_world=0x7f06001c;
         public static final int location=0x7f06001d;
+        public static final int mapView=0x7f06001e;
     }
     public static final class style {
         /** 
